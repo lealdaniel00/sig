a700dc78602b7ce6b250765267d6b307f8286c18
unknown <lealdaniel00@gmail.com>
2025-07-18 18:38:35 -0500

    Se creo el programa

---
+++ MiPrimerPrograma.java
@@ -0,0 +1,12 @@
+public class MiPrimerPrograma{
+	public static void sumar(int a, int b){
+		System.out.println(a+b);
+	}
+	public static int suma(int a, int b){
+		return a+b;
+	}
+	public static void main(String []args){
+		sumar(5,7);
+		System.out.println(suma(6,6));
+	}
+}
